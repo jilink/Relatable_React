fc54e98afe35de8b27e9c57fa21b6fb4c17b4f41
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-08-30 11:11:26 +0200

    project begins with rooter & navbar working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, FormControl, Button, Form } from 'react-bootstrap';
+import {
+      BrowserRouter as Router,
+      Switch,
+      Route,
+      Link,
+    Redirect
+} from "react-router-dom";
+import Home from './Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      return (
+              <Router>
+                  <div>
+                      <Navbar bg="light" variant="light">
+                          <Navbar.Brand href="/en">Relatable</Navbar.Brand>
+                          <Nav className="ml-auto">
+                            <Nav.Link href="/register">Register</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                          </Nav>
+                    </Navbar>
+
+                  {/* A <Switch> looks through its children <Route>s and
+                              renders the first one that matches the current URL. */}
+                  <Switch>
+                    <Route path="/">
+                      <Redirect to="/en" />
+                    </Route>
+                    <Route path="/about">
+                      <Home />
+                    </Route>
+                    <Route path="/users">
+                      <Home />
+                    </Route>
+                    <Route path="/en">
+                      <Home />
+                    </Route>
+                  </Switch>
+                </div>
+              </Router>
+            );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, Nav, FormControl, Button, Form } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
