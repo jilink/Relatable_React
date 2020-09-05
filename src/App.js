d7e377596db0781c68306c372642b21a7b10d8d7
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-09-05 12:18:55 +0200

    indentation

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 import MainRoute from './routes/MainRoute';
 
 function App() {
-      return (
-          <MainRoute></MainRoute>
-      );
+    return (
+        <MainRoute></MainRoute>
+    );
 }
 
 export default App;
--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import {
-      BrowserRouter as Router,
-      Switch,
-      Route,
-      Link,
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
     Redirect
 } from "react-router-dom";
 import Home from '../Home';
 import MainNavbar from '../components/MainNavbar';
 
 function MainRoute() {
-      return (
-              <Router>
-                  <div>
-          <MainNavbar></MainNavbar>
+    return (
+        <Router>
+            <div>
+                <MainNavbar></MainNavbar>
 
-                  {/* A <Switch> looks through its children <Route>s and
-                              renders the first one that matches the current URL. */}
-                  <Switch>
+                <Switch>
                     <Route exact path="/">
-                      <Redirect to="/en" />
+                        <Redirect to="/en" />
                     </Route>
                     <Route path="/about">
-                      <Home />
+                        <Home />
                     </Route>
                     <Route path="/users">
-                      <Home />
+                        <Home />
                     </Route>
                     <Route path="/en">
-                      <Home />
+                        <Home />
                     </Route>
-                  </Switch>
-                </div>
-              </Router>
-            );
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default MainRoute;
