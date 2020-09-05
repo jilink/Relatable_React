e5028219edd78b1cdf7a1994862f7d4bb1060629
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-09-05 15:45:20 +0200

    some cleaning

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 import { I18nProvider, LOCALES } from './i18n';
@@ -6,10 +6,10 @@ import translate from './i18n/translate';
 
 function App() {
     const locale = localStorage.getItem('locale') || LOCALES.ENGLISH
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 import { I18nProvider, LOCALES } from '../i18n';
 import translate from '../i18n/translate';
-
 
 function MainNavbar(){
     function swapLanguage () {
@@ -15,9 +14,10 @@ function MainNavbar(){
             localStorage.setItem('locale', LOCALES.FRENCH);
             window.location.reload(false);
         }
-
     }
+
     const locale = localStorage.getItem('locale') || LOCALES.ENGLISH
+
     return (
         <I18nProvider locale={locale}>
             <Navbar bg="light" variant="light">
