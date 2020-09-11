c5739c726bdcb8dd425f95c8a6b784debe6f9028
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-09-11 19:30:33 +0200

    getting text from firebase

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    apiKey: "AIzaSyANHu56bTwpcSir1_CcMYeScejBxYEbULs",
+    authDomain: "relatable-react.firebaseapp.com",
+    databaseURL: "https://relatable-react.firebaseio.com",
+}
--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -7,26 +7,76 @@ import { Circle } from 'rc-progress';
 import { I18nProvider, LOCALES } from '../i18n';
 import translate from '../i18n/translate';
 
+import * as firebase from 'firebase'
+import config from '../config'
+
 class Browse extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             showResult: false,
             percentage: 0,
+            loading: true,
         };
 
         this.locale = localStorage.getItem('locale') || LOCALES.ENGLISH;
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config)
+        }
+    }
+
+    componentWillMount() {
+        const ref = firebase.database().ref('fr')
+        console.log("coucouuu")
+        ref.on('value', snapshot => {
+            this.setState({
+                post: snapshot.val()[0],
+                loading: false,
+                percentage: snapshot.val()[0].up / (snapshot.val()[0].up + snapshot.val()[0].down) * 100,
+            })
+        })
     }
 
     handleClick(type) {
-        this.setState({
-            showResult: !this.state.showResult,
-            percentage: 25,
-        });
-        console.log("hey", this.state.showResult)
+        this.setState({loading: true})
+        switch(type) {
+            case 'up':
+                this.setState({
+                    showResult: !this.state.showResult,
+                    loading: false,
+                })
+                break;
+            case 'down':
+                this.setState({
+                    showResult: !this.state.showResult,
+                    loading: false,
+                })
+                break;
+            case 'next':
+                const ref = firebase.database().ref('fr')
+                ref.on('value', snapshot => {
+                    this.setState({
+                        showResult: !this.state.showResult,
+                        post: snapshot.val()[1],
+                        percentage: snapshot.val()[1].up / (snapshot.val()[1].up + snapshot.val()[1].down) * 100,
+                        loading: false,
+                    })
+                })
+                break;
+            default:
+                this.setState({
+                    loading: false,
+                })
+                break;
+        }
     }
 
     render(){
+        if (this.state.loading){
+            return (
+                <div> chargement en cours </div>
+            )
+        }
         return (
             <I18nProvider locale={this.locale}>
                 <Container className="mt-5">
@@ -41,29 +91,28 @@ class Browse extends React.Component {
                                 <Card.Body>
                                     <blockquote className="blockquote mb-0">
                                         <p className="text-center">
-                                            {' '}
-                                            LLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.orem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.{' '}
+                                            {this.state.post.text}
                                         </p>
                                         <footer className="blockquote-footer">
-                                            <cite title="Source Title">Anonymous</cite>
+                                            <cite title="Source Title">{this.state.post.username}</cite>
                                         </footer>
                                     </blockquote>
                                 </Card.Body>
                             </Card>
                             <Row className="justify-content-md-center mt-5">
                                 <Col md="auto">
-                                    <Button size="lg" variant="info" className="mr-5" onClick={() => this.handleClick('James')}>{translate("so_relatable")}</Button>{' '}
-                                    <Button size="lg" variant="danger" onClick={() => this.handleClick('James')}>{translate("not_relatable")}</Button>{' '}
+                                    <Button size="lg" variant="info" className="mr-5" onClick={() => this.handleClick('up')}>{translate("so_relatable")}</Button>{' '}
+                                    <Button size="lg" variant="danger" onClick={() => this.handleClick('down')}>{translate("not_relatable")}</Button>{' '}
                                 </Col>
                             </Row>
                         </div>
                     ) : (
                         <div className="justify-content-md-center mt-5 w-25 m-auto">
                             <Circle percent={this.state.percentage} strokeWidth="4" strokeColor="#66ccff" /> 
-                            <small className="text-success">{translate('percentage', {'percentage': 10, 'number': 21})}</small>
+                            <small className="text-success">{translate('percentage', {'percentage': this.state.percentage, 'number': this.state.post.up})}</small>
                             <Row className="justify-content-md-center mt-5">
                                 <Col md="auto">
-                                    <Button size="lg" variant="info" onClick={() => this.handleClick('James')}>{translate("next")}</Button>{' '}
+                                    <Button size="lg" variant="info" onClick={() => this.handleClick('next')}>{translate("next")}</Button>{' '}
                                 </Col>
                             </Row>
                         </div>
