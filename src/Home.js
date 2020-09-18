1844b58b8ce9447d5223685a9fc5fc8bdf4d6d13
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-09-18 19:11:39 +0200

    Login + name base kinda work

--- src/Home.js
+++ src/Home.js
@@ -6,30 +6,74 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import { I18nProvider, LOCALES } from './i18n';
 import translate from './i18n/translate';
 
-function App() {
-    const locale = localStorage.getItem('locale') || LOCALES.ENGLISH
-    return (
-        <I18nProvider locale={locale}>
-            <Container className="mt-5">
-                <Row className="justify-content-center">
-                    <Col xs="auto">
-                        <h1 className="logo"> RELATABLE </h1>
-                    </Col>
-                </Row>
-                <Row className="justify-content-center">
-                    <Col xs="auto">
-                        <h5 className="catchphrase">{translate("catchphrase")}</h5>
-                    </Col>
-                </Row>
-                <Row className="justify-content-center mt-5">
-                    <Col xs="auto">
-                          <Button className="m-3" size="lg" variant="success" href="/browse">{translate("browse")}</Button>{' '}
-                          <Button className="m-3" size="lg" variant="info" href="/create">{translate("create")}</Button>{' '}
-                    </Col>
-                </Row>
-            </Container>
-        </I18nProvider>
-    );
+import config from './config'
+import firebase from 'firebase/app'
+import 'firebase/auth';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.locale = localStorage.getItem('locale') || LOCALES.ENGLISH;
+        this.state = {
+            user: {},
+        }
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config)
+        }
+    }
+
+    componentDidMount(){
+        this.authListener();
+    }
+
+    authListener(){
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log("user", user)
+            if (user) {
+                const update = {
+                    displayName: 'JimmyLeBg',
+                };
+                firebase.auth().currentUser.updateProfile(update);
+                this.setState({user})
+            }
+            else {
+                this.setState({user : null})
+            }
+                
+        })
+    }
+    
+
+    render() {
+        return (
+            <I18nProvider locale={this.locale}>
+                <Container className="mt-5">
+                    <Row className="justify-content-center">
+                        <Col xs="auto">
+                            <h1 className="logo"> RELATABLE </h1>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Col xs="auto">
+                            <h5 className="catchphrase">{translate("catchphrase")}</h5>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-center mt-5">
+                        <Col xs="auto">
+                              <Button className="m-3" size="lg" variant="success" href="/browse">{translate("browse")}</Button>{' '}
+                              <Button className="m-3" size="lg" variant="info" href="/create">{translate("create")}</Button>{' '}
+                        </Col>
+                    </Row>
+                {this.state.user ?
+                        <div> il y a bien un user {this.state.user.displayName} </div>
+                        :
+                        <div> il y a paaaas un user </div>
+                
+                }
+                </Container>
+            </I18nProvider>
+        );
+    }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,24 +6,23 @@ import { Container, Row, Col, Button, Form, Alert} from 'react-bootstrap';
 import { I18nProvider, LOCALES } from '../i18n';
 import translate from '../i18n/translate';
 
+import config from '../config'
 import firebase from 'firebase/app'
 import 'firebase/database';
-import config from '../config'
 
 class Login extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            relatableText: '',
-            username: 'Anonymous Genius',
+            email: '',
+            password: '',
             success: false,
             failure: false,
         };
 
-        this.handleChangeText = this.handleChangeText.bind(this);
-        this.handleChangeUsername = this.handleChangeUsername.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.login = this.login.bind(this);
         this.locale = localStorage.getItem('locale') || LOCALES.ENGLISH;
         this.category = "general"
 
@@ -32,80 +31,33 @@ class Login extends React.Component {
         }
     }
 
-    handleChangeText(event) {
-        this.setState({
-            relatableText: event.target.value,
-        });
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
     }
 
-    handleChangeUsername(event) {
-        this.setState({
-            username: event.target.value,
-        });
-    }
-
-    handleSubmit(event) {
-        const today = new Date();
-        const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        if (!this.state.relatableText){
-            this.setState({failure: true, success: false, relatableText: ''})
-        }
-        else {
-            firebase.database().ref(this.locale +'/' + this.category ).push().set({
-                username: this.state.username,
-                text: this.state.relatableText,
-                up: 0,
-                down: 0,
-                date: date,
-                timestamp: Date.now(),
-                id: Math.random().toString(36).substr(2, 9),
-            })
-                .then((doc) => {
-                    this.setState({success: true, failure:false, relatableText: ''})
-                })
-                .catch((error) => {
-                    console.error(error);
-                })
-        }
-
+    login(event){
         event.preventDefault();
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {}).catch((error) => {
+        console.log(error)
+        });
     }
 
     render() {
         return (
             <I18nProvider locale={this.locale}>
-            {this.state.success ?
-                    <Alert variant="success" className="text-center">
-                        {translate("sent")}
-                    </Alert>
-            : null}
-            {this.state.failure ?
-                    <Alert variant="danger" className="text-center">
-                        {translate("failure")}
-                    </Alert>
-            : null}
                 <Container className="mt-5">
-                    <Row className="justify-content-center mb-5">
-                        <Col xs="auto">
-                            <h2 className="text-info">{translate("create")}</h2>
+                    <Form onSubmit={this.login}>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control name="email" value={this.state.email} onChange={this.handleChange} type="email" placeholder="Enter email" autoComplete="on" />
+                        </Form.Group>
+                        <Form.Group controlId="formGroupPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control name= "password" value={this.state.password} onChange={this.handleChange} type="password" placeholder="Password" autoComplete="on" />
+                        </Form.Group>
+                        <Col sm={3} className="my-1">
+                            <Button type="submit">{translate("login")}</Button>
                         </Col>
-                    </Row>
-                    <Form onSubmit={this.handleSubmit}>
-                        <Row className="d-flex justify-content-center">
-                            <Col sm={7} className="my-1">
-                                <Form.Group>
-                                    <Form.Control maxLength="200" as="textarea" rows="3" value={this.state.relatableText} onChange={this.handleChangeText} />
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row className="d-flex justify-content-center">
-                            <Col sm={4} className="my-1">
-                                <Form.Control className="mb-2" md="4" maxLength="40" value={this.state.username} onChange={this.handleChangeUsername} />
-                            </Col>
-                            <Col sm={3} className="my-1">
-                                <Button type="submit">{translate("submit")}</Button>
-                            </Col>
-                        </Row>
                     </Form>
                 </Container>
             </I18nProvider>
